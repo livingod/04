23e84546da605baa9c8273ffba319b60da2739be
livingod <144990631+livingod@users.noreply.github.com>
2023-10-05 14:29:24 +0900

    04

--- main.c
+++ main.c
@@ -3,11 +3,14 @@
 
 int main(int argc, char *argv[])
 {
-   int a;
-   printf("input the second: ");
-   scanf("%d",&a);
+   int y;
    
-   printf("the time is %d : %d\n",a/60,a%60);
+   printf("input the year: ");
+   scanf("%d",&y);
+   
+   if ((y%4 == 0&&y%100 != 0)||(y%400 == 0))
+      printf("is the year %d the leap year?: %d\n",y,1);
+   else printf("is the year %d the leap year?: %d\n",y,0);
 
   
   system("PAUSE");	
