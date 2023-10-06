7654f893a31f6093dc27127ba49886953f5cc33b
livingod <144990631+livingod@users.noreply.github.com>
2023-10-06 16:34:14 +0900

    05

--- main.c
+++ main.c
@@ -3,16 +3,21 @@
 
 int main(int argc, char *argv[])
 {
-   int y;
+   unsigned int x;
+   int b;
    
-   printf("input the year: ");
-   scanf("%d",&y);
+   printf("input a number: ");
+   scanf("%ui",&x);
    
-   if ((y%4 == 0&&y%100 != 0)||(y%400 == 0))
-      printf("is the year %d the leap year?: %d\n",y,1);
-   else printf("is the year %d the leap year?: %d\n",y,0);
-
+   for(b=0; x!=0; x>>=1)
+   {
+            if (x & 1)
+            {
+                  b++;
+            }
+    }
   
+  printf("The result is: %i\n",b);
   system("PAUSE");	
   return 0;
 }
